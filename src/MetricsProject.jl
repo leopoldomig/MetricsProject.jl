0d871f7b718706782ed317a8bad602ff13e58b6a
Leopoldo Gutierre <leopoldo.mig@gmail.com>
2018-12-17 19:24:11 -0800

    Update MetricsProject.jl

--- src/MetricsProject.jl
+++ src/MetricsProject.jl
@@ -1,5 +1,11 @@
 module MetricsProject
 
-greet() = print("Hello World!")
+using LinearAlgebra, Statistics, NLsolve
+
+# Including the modules
+include("Berry1994.jl")
+
+# Exporting the functions of the modules
+export Berry1994
 
 end # module
